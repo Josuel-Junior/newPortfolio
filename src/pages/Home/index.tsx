c42c4f22c808c2db37cdf8d33156744f376f1ab4
Josuel Junior <108230374+Josuel-Junior@users.noreply.github.com>
2024-12-11 18:51:59 -0300

    commit initial

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,10 @@
+export const Home: React.FC = () => {
+    return (
+        <div>
+            <h1>Hello Word</h1>
+            <h1>Hello Word</h1>
+            <h1>Hello Word</h1>
+            <h1>Hello Word</h1>    
+        </div>
+    );
+};
