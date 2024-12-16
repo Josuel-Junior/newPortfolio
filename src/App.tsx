f589b041bbe56c7467abc7735cf583c1ce3f9d8a
Josuel Junior <108230374+Josuel-Junior@users.noreply.github.com>
2024-12-16 19:21:39 -0300

    navbar navigation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ThemeContext, ThemeProvider } from "@emotion/react"
 import { Outlet } from "react-router-dom"
 import { Light } from "./shared/theme/Light";
 import { ScopedCssBaseline } from "@mui/material";
+import { NavBar } from "./components/NavBar/NavBar";
 
 
 
@@ -9,6 +10,7 @@ function App() {
   return (
 
     <ThemeProvider theme={Light}>
+      <NavBar/>
       <ScopedCssBaseline />
       <Outlet />
     </ThemeProvider>
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,14 @@
-import { AppBar, useTheme } from "@mui/material"
+import { AppBar, Box, Typography, useMediaQuery, useTheme, Toolbar, Tabs, Tab } from "@mui/material"
+import { useNavigate } from "react-router-dom";
+
+
 
 export const NavBar: React.FC = () => {
 
+    const navigate = useNavigate();
     const theme = useTheme();
+
+    const isMatch = useMediaQuery(theme.breakpoints.down("md"));
 
     return (
 
@@ -10,13 +16,56 @@ export const NavBar: React.FC = () => {
             elevation={0}
             sx={{
                 background:
-                     `${theme.palette.background.paper}`,
+                    `${theme.palette.background.paper}`,
                 display: "flex",
                 justifyContent: "center",
                 transition: "background 0.2s ease-in-out",
             }}
         >
+            <Box sx={{ position: "absolute", marginLeft: "60px" }}
 
+            >
+                <Typography>Josuel Junior</Typography>
+            </Box>
+            {isMatch ? (
+                <Box>
+
+                </Box>
+            ) : (
+                <Toolbar>
+                    <Tabs
+                        sx={{ marginLeft: "auto" }}
+
+                        indicatorColor="primary"
+                        textColor="primary"
+                        TabIndicatorProps={{
+                            style: {
+                                backgroundColor: "#296fcd",
+                            },
+                        }}
+                    >
+                        <Tab label="Home" onClick={() => navigate("/")} />
+                        <Tab
+                            label="currículo"
+                            onClick={() => navigate("/")}
+                        />
+                        <Tab
+                            label="Formação"
+                            onClick={() => navigate("/")}
+                        />
+                        <Tab
+                            label="Projetos"
+                            onClick={() => navigate("/")}
+                        />
+                        <Tab
+                            label="Contato"
+                            onClick={() => navigate("/")}
+                        />
+
+                    </Tabs>
+
+                </Toolbar>
+            )}
         </AppBar>
     )
 }
